8c82bcf3e83145676d32159b20265518eea077e0
carlaArdiles <carla. judith.ardiles@gmail.com>
2023-11-15 06:06:19 -0300

    formulario de registro

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require ("path")
+const PORT =3030;
+
+app.use(express.static('./public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./register.html"));
+  });
+
+app.listen(PORT , () => {
+    console.log("Servidor funcionando")
+})
