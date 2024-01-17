2909a6f5f11eb83c5fff8c6aac6526cbefdf4999
Alejandro Vittar <alejandrovittar@gmail.com>
2024-01-16 22:01:53 -0300

    agregado el archivo de rutas para usuarios partiendo del archivo de rutas para productos

---
+++ src/routes/usersRoutes.js
@@ -0,0 +1,29 @@
+//usersRoutes
+
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+// ************ Controller Require ************
+const usersController = require('../controllers/usersControllers');
+
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create', productsController.create); 
+router.post('/', productsController.store); 
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/detail/:id', productsController.detail); 
+
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+router.put('/:id', productsController.update); 
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/:id/delete', productsController.destroy); 
+
+module.exports = router;
