722d55580b68d9f015500fd4bfe6766ac107b4ab
ignaitu93 <ignaitu93@gmail.com>
2024-01-23 19:29:24 -0300

    autenticacion y lectura de los usuarios

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -3,11 +3,16 @@ const fs = require("fs");
 
 const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
 const controller = {
   home: (req, res) => {
     const masVendidos = products.slice(0, 5);
     const destacados = products.slice(-5);
-    res.render("index", { masVendidos, destacados });
+
+    // Obtener información del usuario si está autenticado
+    const usuario = req.session.usuario;
+
+    res.render("index", { masVendidos, destacados, usuario });
   },
 
   showCartPage: (req, res) => {
@@ -22,6 +27,10 @@ const controller = {
     res.render("register", { pageTitle: "Registro de Usuario" });
   },
 
+  renderHomePage: (req, res) => {
+    const usuario = req.session.usuario;
+    res.render('index', { usuario });  // Asegúrate de incluir 'usuario' en los datos
+  },
 };
 
-module.exports =  controller ;
+module.exports = controller;
