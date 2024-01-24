6d90227c08ac82bbf76fd68042d76c3015b017af
Tamav <tamav.peinetti@gmail.com>
2024-01-24 15:56:38 -0300

    retrospectivaS4

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const withLogin = (req, res, next) => {
     if(req.session.user === undefined){
         res.redirect('/'); //Redirige al usuario a la pagina de inicio de sesion
+        return;
     }
   next();
 };
@@ -8,6 +9,7 @@ const withLogin = (req, res, next) => {
 const withoutLogin = (req, res, next) => {
     if(req.session.user){
         res.redirect('/'); //Redirige al usuario al perfil si ya esta autentificado
+        return;
     }
   next();
 };
