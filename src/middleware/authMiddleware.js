bdb519fff59f57d41acbbf0c5f1450ef885ee5b4
Tamav <tamav.peinetti@gmail.com>
2024-01-24 15:10:10 -0300

    Armando Estructura

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,8 +1,14 @@
 const withLogin = (req, res, next) => {
+    if(req.session.user === undefined){
+        res.redirect('/'); //Redirige al usuario a la pagina de inicio de sesion
+    }
   next();
 };
 
 const withoutLogin = (req, res, next) => {
+    if(req.session.user){
+        res.redirect('/'); //Redirige al usuario al perfil si ya esta autentificado
+    }
   next();
 };
 
